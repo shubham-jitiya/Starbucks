a2b72a93d27c59e23300521e287ad5e7b3f032f2
Jitiya Shubham <shubham.j@simformsolutions.com>
2023-02-15 12:01:50 +0530

    Initialising project

---
+++ demo_signup/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  demo_signup
+//
+//  Created by Shubham Jitiya on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //label.text = "Test"
+        //      ackward
+    }
+    
+    
+}
+
+
+
